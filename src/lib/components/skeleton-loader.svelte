8d66500095b1039f29fef35f4ee1290251ececdc
Joseph Rios <blankworldwide@gmail.com>
2021-11-18 15:45:24 -0800

    formats

--- src/lib/components/skeleton-loader.svelte
+++ src/lib/components/skeleton-loader.svelte
@@ -1,34 +1,33 @@
 <script lang="ts">
-    export let count = 1;
-    export let width = 100;
-    const elements = [];
-    for (let i = 0; i < count; i++) {
-      elements.push(i);
-    }
-  </script>
-  
-  {#each elements as _}
-    <span style="width:{width}%;"> &zwnj; </span>
-  {/each}
-  
-  <style>
-    span {
-      background-color: #eee;
-      background-image: linear-gradient(90deg, #eee, #f5f5f5, #eee);
-      background-size: 200px 100%;
-      background-repeat: no-repeat;
-      border-radius: 4px;
-      display: inline-block;
-      line-height: 1;
-      animation: loader 1.2s ease-in-out infinite;
-    }
-    @keyframes loader {
-      0% {
-        background-position: -200px 0;
-      }
-      100% {
-        background-position: calc(200px + 100%) 0;
-      }
-    }
-  </style>
-  
+	export let count = 1;
+	export let width = 100;
+	const elements = [];
+	for (let i = 0; i < count; i++) {
+		elements.push(i);
+	}
+</script>
+
+{#each elements as _}
+	<span style="width:{width}%;"> &zwnj; </span>
+{/each}
+
+<style>
+	span {
+		background-color: #eee;
+		background-image: linear-gradient(90deg, #eee, #f5f5f5, #eee);
+		background-size: 200px 100%;
+		background-repeat: no-repeat;
+		border-radius: 4px;
+		display: inline-block;
+		line-height: 1;
+		animation: loader 1.2s ease-in-out infinite;
+	}
+	@keyframes loader {
+		0% {
+			background-position: -200px 0;
+		}
+		100% {
+			background-position: calc(200px + 100%) 0;
+		}
+	}
+</style>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,10 +1,11 @@
 <script type="ts">
-    import SkeletonLoader from "$lib/components/skeleton-loader.svelte";
+	import SkeletonLoader from '$lib/components/skeleton-loader.svelte';
 </script>
 
-<p>Hello welcome to my <span style='display:inline'> <SkeletonLoader width={10}/>  </span> this is just an example on a some of the few things I have been working on at Temporal</p>
+<p>
+	Hello welcome to my <span style="display:inline"> <SkeletonLoader width={10} /> </span> this is just
+	an example on a some of the few things I have been working on at Temporal
+</p>
 
 <style>
-
-
 </style>
